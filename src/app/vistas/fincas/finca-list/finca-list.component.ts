92d95809fdfe81d03aff4fdd3f61de99c9a01da4
Oscar Mauricio Hevilla <mauhevilla@gmail.com>
2018-05-08 08:23:35 -0300

    codifdifico fincas abm terminado

--- src/app/vistas/fincas/finca-list/finca-list.component.ts
+++ src/app/vistas/fincas/finca-list/finca-list.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+// import Service
+import {FincasService } from '../../../services/fincas.service';
+// import la clases
+import { FincaDb } from '../../../models/finca-db';
+// impor ngForm
+import { NgForm} from '@angular/forms';
+// impor toaster para mensajes
+import { ToastrService} from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-finca-list',
@@ -7,9 +16,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FincaListComponent implements OnInit {
 
-  constructor() { }
+  fincaList : FincaDb[];
+
+  constructor(private servicioFincas:FincasService,
+    private toast : ToastrService ) { }
 
   ngOnInit() {
+    return this.servicioFincas.getFincas()
+    .snapshotChanges()
+    .subscribe(item => {
+      this.fincaList=[];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"]=element.key;
+        this.fincaList.push(x as FincaDb);
+      });
+    });
+  }
+
+  onEdit(finca : FincaDb){
+    this.servicioFincas.fincaSelect = Object.assign({},finca) ;
+  }
+
+  onDelete($key:string){
+    if(confirm('Esta seguro de querer Eliminarlo ?')){
+      this.servicioFincas.deletFinca($key);
+      this.toast.success('Successfull Operation','Finca Elimnada ...');
+    }
+    
   }
 
 }
